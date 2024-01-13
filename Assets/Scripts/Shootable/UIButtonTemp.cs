bfc3cc63b2355bb74203f7f51a611812a336c7d4
bananaizg0d <efebektas@yahoo.com>
2024-01-13 14:52:13 +0300

    menuBaseScene

---
+++ Assets/Scripts/Shootable/UIButtonTemp.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtonTemp : Shootable_Base
+{
+    enum ButtonType {Play, Options, HowToPlay, Quit};
+
+
+    public override void OnShot()
+    {
+        base.OnShot();
+
+    }
+
+
+    public void PlayButton()
+    {
+
+    }
+
+    public void QuitButton()
+    {
+
+    }
+}
