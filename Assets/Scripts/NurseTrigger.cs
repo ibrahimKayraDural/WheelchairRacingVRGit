ccc67ca792efa0cbb0e82f4e5c29d98fb0a7e4bc
kayra <157609274+ibrahimKayraDural@users.noreply.github.com>
2024-05-30 15:25:51 +0300

    nurse footsteps (experimental)

--- Assets/Scripts/NurseTrigger.cs
+++ Assets/Scripts/NurseTrigger.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class NurseTrigger : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] NurseController NC;
     [SerializeField] GameObject Song;
+    [SerializeField] UnityEvent OnTriggered;
 
     private void OnTriggerEnter(Collider other)
     {
         NC.GOOO();
-
         Instantiate(Song, transform.position, Quaternion.identity);
+        OnTriggered?.Invoke();
 
         Destroy(gameObject);
     }
